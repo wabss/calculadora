b5cf46ec134e4db091fd633aebaa471856eeeca7
wabss <jesusssa2005@gmail.com>
2025-02-05 22:35:46 -0500

    Version 1.0.0

---
+++ nose.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class nose{
+    public static void main(String[] args){
+        try(Scanner sc = new Scanner(System.in)){
+            String input = sc.nextLine();
+            String[] inputCatcher = input.split(" ");
+            ArrayList<String> inputProcess = new ArrayList<>();
+
+            for (String element : inputCatcher) { 
+                    inputProcess.add(element);
+            }
+
+            int lenght = inputProcess.size();
+            int result = 0;
+
+            for(int i = 0; i<lenght;i++){
+                if(inputProcess.get(i).equals("+") && i > 0 && i < lenght - 1){
+                    int prev = Integer.parseInt(inputProcess.get(i-1));
+                    int next = Integer.parseInt(inputProcess.get(i+1));
+                    result += prev + next;
+                } else if(inputProcess.get(i).equals("-") && i > 0 && i < lenght - 1){
+                    int prev = Integer.parseInt(inputProcess.get(i-1));
+                    int next = Integer.parseInt(inputProcess.get(i+1));
+                    result += prev - next;
+                } else {
+                    continue;
+                }
+            }
+            System.out.println(result);
+        }
+    }
+}
